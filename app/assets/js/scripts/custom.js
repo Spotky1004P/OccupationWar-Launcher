ccf17798d0b04011f05633f2b605780029e35229
Spotky1004 <spotky1004@gmail.com>
2022-11-28 01:29:43 +0900

    News implement

--- app/assets/js/scripts/custom.js
+++ app/assets/js/scripts/custom.js
@@ -6,12 +6,21 @@ document.getElementById("launch_button").addEventListener("click", () => {
 });
 
 const newsJsonUrl = "https://raw.githubusercontent.com/Spotky1004P/OccupationWar-Launcher/main/news.json";
+const newsItems = {
+  "news1": document.getElementById("landing__news1"),
+  "news2": document.getElementById("landing__news2"),
+  "news3": document.getElementById("landing__news3")
+};
 request({
   url: newsJsonUrl,
   timeout: 2500
 }, (err, res, body) => {
   if (!err) {
-    console.log(JSON.parse(body));
+    const newsObj = JSON.parse(body);
+    for (const itemName in newsItems) {
+      if (!(itemName in newsObj)) continue;
+      newsItems[itemName].style.backgroundImage = `url(${newsObj[itemName].image})` 
+    }
   } else {
 
   }
