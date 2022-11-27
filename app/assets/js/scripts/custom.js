350d25aaabda800eabe5409d42448dd4257f5737
Spotky1004 <spotky1004@gmail.com>
2022-11-28 02:25:26 +0900

    Create hrefWorker and use

--- app/assets/js/scripts/custom.js
+++ app/assets/js/scripts/custom.js
@@ -1,9 +1,18 @@
+const electron = require('electron');
 const request = require('request');
 
 document.getElementById("launch_button").addEventListener("click", () => {
   document.getElementById("landing__content__before").style.display = "none";
   document.getElementById("landing__content__after").style.display = "";
 });
+
+function hrefWorker(el) {
+  const link = el.getAttribute("href");
+  console.log(el, link);
+  el.addEventListener("click", () => {
+    electron.shell.openExternal(link);
+  })
+}
 
 const newsJsonUrl = "https://raw.githubusercontent.com/Spotky1004P/OccupationWar-Launcher/main/news.json";
 const newsItems = {
@@ -19,9 +28,16 @@ request({
     const newsObj = JSON.parse(body);
     for (const itemName in newsItems) {
       if (!(itemName in newsObj)) continue;
-      newsItems[itemName].style.backgroundImage = `url(${newsObj[itemName].image})` 
+      const newsItem = newsObj[itemName];
+      const el = newsItems[itemName];
+      el.style.backgroundImage = `url(${newsItem.image})`;
+      el.setAttribute("href", newsItem.link);
+      hrefWorker(newsItems[itemName]);
     }
   } else {
-
+    console.log("Error occured while loading news :(")
   }
 });
+
+hrefWorker(document.getElementById("langing__tabsHompage"));
+hrefWorker(document.getElementById("langing__tabsDiscord"));
