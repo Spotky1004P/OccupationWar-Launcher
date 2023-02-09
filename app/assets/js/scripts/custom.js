384ca1d4f7cdad961f9a1d8de7c2803f613990e0
Spotky1004 <spotky1004@gmail.com>
2023-02-09 13:35:24 +0900

    v1.0.9

--- app/assets/js/scripts/custom.js
+++ app/assets/js/scripts/custom.js
@@ -8,37 +8,50 @@ document.getElementById("launch_button").addEventListener("click", () => {
 
 function hrefWorker(el) {
   const link = el.getAttribute("href");
-  console.log(el, link);
   el.addEventListener("click", () => {
+    if (link === " ") return;
     electron.shell.openExternal(link);
   })
 }
 
-const newsJsonUrl = "http://snics-client.kro.kr/news.json";
+// const newsJsonUrl = "http://snics-client.kro.kr:8080/news.json";
 const newsItems = {
   "news1": document.getElementById("landing__news1"),
   "news2": document.getElementById("landing__news2"),
   "news3": document.getElementById("landing__news3")
 };
+
 request({
-  url: newsJsonUrl,
+  url: "https://raw.githubusercontent.com/Spotky1004P/OccupationWar-Launcher-Distribution/main/distribution.json",
   timeout: 2500
 }, (err, res, body) => {
-  if (!err) {
-    const newsObj = JSON.parse(body);
-    console.log("News recived!", JSON.stringify(newsObj));
-    for (const itemName in newsItems) {
-      if (!(itemName in newsObj)) continue;
-      const newsItem = newsObj[itemName];
-      const el = newsItems[itemName];
-      el.style.backgroundImage = `url(${newsItem.image})`;
-      el.setAttribute("href", newsItem.link);
-      hrefWorker(newsItems[itemName]);
-    }
+  if(!err){
+    const rawDistro = JSON.parse(body);
+    const newsUrl = rawDistro?.newsUrl ?? "http://snics-client.kro.kr:8080/news.json";
+    request({
+      url: newsUrl,
+      timeout: 2500
+    }, (err, res, body) => {
+      if (!err) {
+        const newsObj = JSON.parse(body);
+        console.log("News recived!", JSON.stringify(newsObj));
+        for (const itemName in newsItems) {
+          if (!(itemName in newsObj)) continue;
+          const newsItem = newsObj[itemName];
+          const el = newsItems[itemName];
+          el.style.backgroundImage = `url(${newsItem.image})`;
+          el.setAttribute("href", newsItem.link);
+          hrefWorker(newsItems[itemName]);
+        }
+      } else {
+        console.log("Error occured while loading news :(")
+      }
+    });
   } else {
-    console.log("Error occured while loading news :(")
+    console.log("Error occured while loading distro :(", err)
+    return
   }
-});
+})
 
 hrefWorker(document.getElementById("langing__tabsHompage"));
 hrefWorker(document.getElementById("langing__tabsDiscord"));
